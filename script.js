81a24f62f4e12e905a9893fcfdd56c483724a46a
Augusto Arce Solis <ticocuenta001@gmail.com>
2022-10-09 21:43:13 -0600

    Adding explotions, collitions and killing enemies

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 /**TODO --This list is either here because I do not have time or my mental abilities--
  * - Object pool (must work for any array object), need some reviews and some refactoring
+ * - I really need to refactor a lot of thing. the variables are a mess, like, the objects need to grab the global variables from the Game class, not from outside.
  */
 
 const canvas = document.getElementById("canvas1");
@@ -16,6 +17,7 @@ let canvasPosition = canvas.getBoundingClientRect();
 /** Frames and velocity */
 
 let gameFrame = 0;
+let staggerFrames = 2;
 let gameSpeed = 7 ;
 let movementSpeed = 50;
 let lastTime = 1;
@@ -36,6 +38,98 @@ enemy1Img.src = 'assets/enemies/en_19.png';
 
 const playerNormalProjectileImg = new Image();
 playerNormalProjectileImg.src = 'assets/shoots/shoot_5.png';
+
+
+
+/** test space */
+
+/** end test space */
+
+
+
+class Game {
+    constructor(ctx, width, height, gameFrame, staggerFrames){
+        this.ctx = ctx;
+        this.width = width; 
+        this.height = height;
+        this.enemies = [];
+        this.enemyInterval = 1000;
+        this.enemyTimer = 0;
+        this.gameFrame = gameFrame;
+        this.staggerFrames = staggerFrames;
+    }
+    
+    update(object){
+        if(object.length){
+            object.forEach(object => object.update());
+        }else{
+            object.update();
+        }
+    }
+    
+    timeCounterEnemies(deltatime, enemyPool){
+        if(this.enemyTimer > this.enemyInterval){
+            this.#addNewEnemy(enemyPool);
+            this.enemyTimer = 0;
+        }else{
+            this.enemyTimer += deltatime;
+        }
+    }
+    
+    draw(object){
+        if(object.length){
+            object.forEach(object => object.draw());
+        }else{
+            object.draw();
+        }
+    }
+
+
+    detectCollition(player, enemies, projectiles, explotions){
+        var playerHitbox = {x : player.x - player.width / 4, y : player.y - player.height / 4, width : player.width / 2, height : player.height / 2};
+        enemies.forEach((enemy) => {
+            var enemeyHitbox = {x : enemy.x - enemy.width / 4, y : enemy.y - enemy.height / 4, width : enemy.width / 2, height : enemy.height / 2};
+
+            this.analiseCollition(playerHitbox, enemeyHitbox);
+
+            projectiles.forEach((projectile) => {
+                var projectileHitbox = {x : projectile.x - projectile.width / 4, y : projectile.y - projectile.height / 4, width : projectile.width / 2, height : projectile.height / 2};
+                if(this.analiseCollition(enemeyHitbox, projectileHitbox)){
+                    var explotion = explotions.getDeletedElement();
+                    explotion.x = enemy.x;
+                    explotion.y = enemy.y;
+                    explotions.releaseElement(explotion);
+                    enemy.alive = false;
+                    projectile.alive = false;
+                }
+            });
+
+        })
+    }
+
+    
+    setGameframe(gameFrame){
+        this.gameFrame = gameFrame;
+    }
+    
+    #addNewEnemy(enemyPool){
+        var enemy = enemyPool.getDeletedElement();
+        if (enemy){
+            enemyPool.releaseElement(enemy);
+        }
+    }
+
+    analiseCollition(hitbox1, hitbox2){
+        if(hitbox1.x < hitbox2.x  + hitbox2.width  && 
+            hitbox1.x + hitbox1.width > hitbox2.x   &&
+            hitbox1.y < hitbox2.y  + hitbox2.height &&
+            hitbox1.y + hitbox1.height > hitbox2.y
+         ){
+            return true;
+         }
+         return false;
+    }
+}
 
 /** Layer class for the parallax efect.
  * @image the image that the layer will render
@@ -72,7 +166,9 @@ constructor(image, timeModifier){
  * @x the initial x position
  * @y the initial y position
  * @image the image (spaceship) of the player
- * @
+ * @spriteWidth the width of the image
+ * @spriteHeight the width of the image
+ * @game the class of the game
  */
 
 class Player {
@@ -94,50 +190,7 @@ class Player {
 
     draw(){
         this.game.ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x - this.width / 2 , this.y - this.height / 2, this.width,  this.height);
-    }
-}
-
-
-class Game {
-    constructor(ctx, width, height){
-        this.ctx = ctx;
-        this.width = width; 
-        this.height = height;
-        this.enemies = [];
-        this.enemyInterval = 1000;
-        this.enemyTimer = 0;
-    }
-
-    update(object){
-        if(object.length){
-            object.forEach(object => object.update());
-        }else{
-            object.update();
-        }
-    }
-
-    timeCounterEnemies(deltatime, enemyPool){
-        if(this.enemyTimer > this.enemyInterval){
-            this.#addNewEnemy(enemyPool);
-            this.enemyTimer = 0;
-        }else{
-            this.enemyTimer += deltatime;
-        }
-    }
-
-    draw(object){
-        if(object.length){
-            object.forEach(object => object.draw());
-        }else{
-            object.draw();
-        }
-    }
-
-    #addNewEnemy(enemyPool){
-        var enemy = enemyPool.getDeletedElement();
-        if (enemy){
-            enemyPool.releaseElement(enemy);
-        }
+        this.game.ctx.strokeRect(this.x - this.width / 4 , this.y - this.height / 4, this.width / 2 , this.height / 2);
     }
 }
 
@@ -146,7 +199,7 @@ class Enemy {
         this.game = game;
         this.x = this.game.width; 
         this.y = Math.random() * this.game.height;
-        this.alive = true;
+        this.alive = false;
         this.image = image;
         this.spriteWidth = spriteWidth; 
         this.spriteHeight = spriteHeight;
@@ -164,6 +217,7 @@ class Enemy {
 
     draw(){
         this.game.ctx.drawImage(this.image, 0, 0, this.spriteWidth, this. spriteHeight, this.x - this.width / 2 , this.y - this.height / 2, this.width,  this.height);
+        this.game.ctx.strokeRect(this.x - this.width / 4 , this.y - this.height / 4, this.width / 2 , this.height / 2);
     }
 }
 
@@ -172,7 +226,7 @@ class Projectile {
         this.game = game;
         this.x = x;
         this.y = y;
-        this.alive = true;
+        this.alive = false;
         this.image = image;
         this.spriteWidth = spriteWidth; 
         this.spriteHeight = spriteHeight;
@@ -191,6 +245,7 @@ class Projectile {
 
     draw(){
         this.game.ctx.drawImage(this.image, 0, 0, this.spriteWidth, this. spriteHeight, this.x - this.width / 2 , this.y - this.height / 2, this.width,  this.height);
+        this.game.ctx.strokeRect(this.x - this.width / 4 , this.y - this.height / 4, this.width / 2 , this.height / 2);
     }
 }
 
@@ -211,6 +266,7 @@ class ObjectPool {
         .fill(0)
         .map(() => this.createElement());
         this.game = game;
+      this.aliveElements = new Array(initialSize).fill(0);  
     }
 
     createElement() {
@@ -218,14 +274,21 @@ class ObjectPool {
       return data;
     }
 
-    getElement() {
+    updateAndDrawAliveElements() {  
+        this.aliveElements = [];
       for (let i = 0; i < this.#poolArray.length; i++) {
         if (this.#poolArray[i].alive === true) {
             this.game.update(this.#poolArray[i]);
             this.game.draw(this.#poolArray[i]);
+            this.aliveElements.push(this.#poolArray[i]);
         }
       }
     }
+
+    getAliveElements(){
+        return this.aliveElements;
+    }
+
     getDeletedElement(){
         for (let i = 0; i < this.#poolArray.length; i++) {
             if (!this.#poolArray[i].alive) {
@@ -241,25 +304,66 @@ class ObjectPool {
 
 
 
+  class Explotion{
+    constructor(spriteWidth, spriteHeight, game, x = 50, y = 50, image = new Image()) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.alive = false;
+        this.image = image;
+        this.image.src = 'assets/explosion/exb_000.png';
+        this.spriteWidth = spriteWidth; 
+        this.spriteHeight = spriteHeight;
+        this.width = spriteWidth * 0.20;
+        this.height = spriteHeight * 0.20;
+        this.imageNumber = 0;
+    }
+
+    update(){
+        if(this.imageNumber < 10 ){
+            this.image.src = `assets/explosion/exb_00${this.imageNumber}.png`;
+        }else if(this.imageNumber < 42){
+            this.image.src = `assets/explosion/exb_0${this.imageNumber}.png`;
+        }else {
+            this.alive = false;
+        }
+        this.imageNumber++;
+        this.y += 10;
+    }
+
+    draw(){
+        this.game.ctx.drawImage(this.image, 0, 0, this.spriteWidth, this. spriteHeight, this.x - this.width / 2 , this.y - this.height / 2, this.width,  this.height);
+    }
+  }
+
+
+
 /** This will happen once the page is loading */
 
 window.addEventListener('load', function() {
     
     /** Initialize objects */
 
-    /**objects*/
-    const game = new Game(ctx, canvas.width, canvas.height);
+    
+    /** image sizes*/
     const playerImgWidh = 894;
     const playerImgHeight = 930;
     const enemyImgWidh = 651;
     const enemyImgHeight = 612;
     const nomalProjectileWidh = 194;
     const normalProjectileHeight = 888;
+    const explosionImgWidh = 697;
+    const explosionImgHeight = 620;
+    
+    /**objects*/
+
     const stars = new Layer(spaceBackground, 0.5);
     const asteroits = new Layer(rocks, 1); 
     const background = [stars, asteroits];
+    const game = new Game(ctx, canvas.width, canvas.height, gameFrame, staggerFrames);
     const player = new Player( 200, game.height - playerImgHeight - 50 , playerImg, playerImgWidh, playerImgHeight, game);
     const playerNomalShot = new Projectile(playerNormalProjectileImg,nomalProjectileWidh,normalProjectileHeight,game,-50, -50);
+    const explotion = new Explotion(explosionImgWidh, explosionImgHeight, game);
 
     /**Object pools */
     
@@ -271,26 +375,38 @@ window.addEventListener('load', function() {
     };
     const enemyPool = new ObjectPool(createEnemyFunc, resetEnemyFunc, 20, game);
 
-    const createNormalProjectileFunc = () => new Projectile(playerNormalProjectileImg,nomalProjectileWidh,normalProjectileHeight,game, player.x, player.y);
+    const createNormalProjectileFunc = () => new Projectile(playerNormalProjectileImg, nomalProjectileWidh, normalProjectileHeight, game);
     const resetNormalProjectileFunc = (shoot) => {
         shoot.x = player.x;
         shoot.y = player.y;
         return shoot;
     };
+
     const normalProjectilePool = new ObjectPool(createNormalProjectileFunc, resetNormalProjectileFunc, 10, game);
+
+
+    const createExplotionFunc = () => new Explotion(explosionImgWidh, explosionImgHeight, game);
+    const resetExplotionFunc = (explotion) => {
+        explotion.imageNumber = 0;
+        explotion.image.src = `assets/explosion/exb_00${explotion.imageNumber}.png`;
+        return explotion;
+    };
+
+    const explotionsPool = new ObjectPool(createExplotionFunc, resetExplotionFunc, 20, game);
 
 
     /* Events listeners */
 
+    /**space ship movement */
     this.window.addEventListener('mousemove', function(e){
         let positionX = e.x - canvasPosition.x;
         let positionY = e.y - canvasPosition.y;
         player.update(positionX, positionY);
     });
 
+    /** shoot event */
     this.window.addEventListener('click', function(e){
         var shoot = normalProjectilePool.getDeletedElement();
-
         normalProjectilePool.releaseElement(shoot);
     });
 
@@ -302,14 +418,23 @@ window.addEventListener('load', function() {
         ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
         const deltatime = timeStamp - lastTime;
         lastTime = timeStamp;
+
         game.update(background);
-        game.update(playerNomalShot);
         game.draw(background);
-        normalProjectilePool.getElement();
+
+
+        normalProjectilePool.updateAndDrawAliveElements();
+
+        explotionsPool.updateAndDrawAliveElements();
+
         game.draw(player);
-        enemyPool.getElement();
-        game.timeCounterEnemies(deltatime, enemyPool)
-        gameFrame++;
+
+        enemyPool.updateAndDrawAliveElements();
+        game.timeCounterEnemies(deltatime, enemyPool);
+
+        game.detectCollition(player, enemyPool.getAliveElements(), normalProjectilePool.getAliveElements(), explotionsPool);
+
+        game.setGameframe(gameFrame++);
         requestAnimationFrame(animate);
     }
     animate(0);
